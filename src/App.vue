0f88271088df96af372b8d99846114047f4f2dc4
poominat <68525293+poominat@users.noreply.github.com>
2020-08-28 17:53:19 +0700

    01

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br />
+    <img class="rounded-circle" src="https://i.pinimg.com/originals/e0/4a/1b/e04a1b6e6b273caa416f0ced917ac494.jpg" width="20%" />
+    <HelloWorld msg = "Battle Fighters" />
+    <br />
+    
+    <div>
+      <botton />
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import botton from "./components/botton.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    botton,
+  },
+};
 </script>
 
 <style>
 #app {
+  background-image: url(https://data.photofunky.net/output/image/d/c/4/4/dc44a3/photofunky.gif);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:azure;
 }
 </style>
---
+++ src/components/botton.vue
@@ -0,0 +1,210 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-sm">
+        <p style="font-size: 150%">{{aplayer.name}}</p>
+        <p>
+          <img v-bind:style="{width: aplayer.hp + 'px'}" :src="hp1" alt height="20px" />
+        </p>
+        <p style="font-size: 150%">{{thp}}{{aplayer.hp}}</p>
+        <p>
+          <img style="width:50%" :src="aplayer.image" />
+        </p>
+      </div>
+      <!-- Botton -->
+      <div class="col-sm">
+        <div class="row">
+          <div class="col">
+            <div class="btn-group" role="group" aria-label="Basic example">
+    <button v-bind:disabled="end" class="btn btn-primary" @click="start()">Start</button>
+    <button v-bind:disabled="end" class="btn btn-light" @click="attack()">Attack</button>
+    <button v-bind:disabled="end" class="btn btn-danger" @click="special()">SpecialAttack</button>
+    <button class="btn btn-success" @click="reset()">Restart</button>
+</div>
+          </div>
+          <br /> 
+          <!-- VS -->
+          <center><img
+            src="https://media2.giphy.com/media/TiJfX5nRSnvshdijyb/source.gif"
+            width="80%"
+          />
+          </center>
+         </div>
+        <div class="row">
+          <div class="col-sm"></div>
+          <div class="col-sm">
+            <div id="score">
+              <!-- WIN -->
+              <div v-if="amonster.hp <= 0"><img
+            src="https://media3.giphy.com/media/jsGz81YPCgw9YSliV0/giphy.gif"
+            width="100%"
+          /></div>
+              <!-- LOST -->
+              <div v-else-if="aplayer.hp <= 0"><img
+            src="https://lh3.googleusercontent.com/proxy/XdqGeg8FNf6nuuaVkxta-HnCPKjYUXRYhkDeRKq5ufZjGVYDujCpCL1NgiiDVGHjTJAqab6G5CUPIIuot_ROQuwRaUo_VsyC"
+            width="100%"
+          /></div>
+            </div>
+          </div>
+          <div class="col-sm"></div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <p style="font-size: 150%">{{amonster.name}}</p>
+        <p>
+          <img v-bind:style="{width: amonster.hp + 'px'}" :src="hp2" alt height="15px" />
+        </p>
+        <p style="font-size: 150%">{{thp}}{{amonster.hp}}</p>
+        <p>
+          <img style="width:50%" :src="amonster.image" />
+        </p>
+      </div>
+    </div>
+    
+      
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      thp: "HP:",
+      end: false,
+      hp2:
+        "https://img.pngio.com/health-bar-png-100-images-in-collection-page-3-health-bar-png-1200_900.png",
+      hp1:
+        "https://img.pngio.com/health-bar-png-pictures-trzcacakrs-health-bar-png-1190_1120.png",
+      aplayer: { name: "", hp: 1, image: "", hp1: "" },
+      player: [
+        {
+          name: "Zinogre",
+          hp: 380,
+          image:
+            "http://www.pngmart.com/files/7/Monster-Hunter-World-PNG-File.png",
+        },
+        {
+          name: "Iron man",
+          hp: 330,
+          image:
+            "http://www.pngmart.com/files/3/Iron-Man-PNG-File.png",
+        },
+        {
+          name: "Rey Skywalker",
+          hp: 130,
+          image:
+            "http://www.pngmart.com/files/12/Star-Wars-The-Rise-Of-Skywalker-PNG-Photos.png",
+        },
+        {
+          name: "Batman",
+          hp: 330,
+          image:
+            "http://www.pngmart.com/files/5/Batman-Arkham-City-PNG-Free-Download.png",
+        },
+        {
+          name: "Terminator",
+          hp: 500,
+          image: "http://www.pngmart.com/files/2/Terminator-PNG-Transparent-Picture.png",
+        },
+        {
+          name: "Hulk",
+          hp: 250,
+          image: "http://www.pngmart.com/files/2/Hulk-PNG-Photos.png",
+        },
+        {
+          name: "Godzilla",
+          hp: 500,
+          image:
+            "https://www.freepngimg.com/thumb/godzilla/3-2-godzilla-free-download-png.png",
+        },
+        
+         
+      ],
+      amonster: { name: "", hp: 1, image: "", hp1: "" },
+      monster: [
+        
+        {
+          name: "Chicken Minecraft",
+          hp: 120,
+          image:
+            "https://www.freepngimg.com/thumb/minecraft/1-2-minecraft-chicken-png.png",
+        },
+        {
+          name: "Sesshomaru",
+          hp: 215,
+          image:
+            "http://www.pngmart.com/files/4/Inuyasha-PNG-Transparent-Picture.png",
+        },
+        {
+          name: "Reborn",
+          hp: 325,
+          image: "http://www.pngmart.com/files/5/Reborn-PNG-HD.png",
+        },
+        {
+          name: "Dah'ren Mohran",
+          hp: 385,
+          image: "http://www.pngmart.com/files/4/Sand-Monster-PNG-Photos.png",
+        },
+        {
+          name: "Monkey D Luffy",
+          hp: 225,
+          image: "http://www.pngmart.com/files/2/Monkey-D-Luffy-PNG-Photos.png",
+        },
+        {
+          name: "Deidara",
+          hp: 230,
+          image:
+            "http://www.pngmart.com/files/13/Naruto-Akatsuki-PNG-Transparent.png",
+        },
+        
+      ],
+      pmax: "",
+      mmax: "",
+    };
+  },
+  methods: {
+    randomno: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    start: function () {
+      this.aplayer = this.player[this.randomno(1, 7) - 1];
+      this.amonster = this.monster[this.randomno(1, 7) - 1];
+    },
+    attack: function () {
+      this.pmax = Math.floor(Math.random() * 10 + 4);
+      this.amonster.hp = this.amonster.hp - this.pmax;
+      this.mmax = Math.floor(Math.random() * 10 + 4);
+      this.aplayer.hp = this.aplayer.hp - this.mmax;
+      if (this.aplayer.hp <= 0) {
+        this.aplayer.hp = 0;
+        this.end = true;
+      } else if (this.amonster.hp <= 0) {
+        this.amonster.hp = 0;
+        this.end = true;
+      }
+    },
+    special: function () {
+      this.pmax = Math.floor(Math.random() * 15 + 6);
+      this.amonster.hp = this.amonster.hp - this.pmax;
+      this.mmax = Math.floor(Math.random() * 15 + 6);
+      this.aplayer.hp = this.aplayer.hp - this.mmax;
+      if (this.aplayer.hp <= 0) {
+        this.aplayer.hp = 0;
+        this.end = true;
+      } else if (this.amonster.hp <= 0) {
+        this.amonster.hp = 0;
+        this.end = true;
+      }
+    },
+    reset: function () {
+      window.location.reload();
+    },
+  },
+};
+</script>
+<style>
+#score {
+  font-size: 300%;
+  color: aliceblue;
+}
+</style>
